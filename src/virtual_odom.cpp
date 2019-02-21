cd57a7fb730f0d41e7f901f9dab1d8e4cc5f9339
lucasilvassis <lucasilvassis@hotmail.com>
2019-02-21 08:51:44 +0000

    Odom virutal zuada

--- src/virtual_odom.cpp
+++ src/virtual_odom.cpp
@@ -1,11 +1,10 @@
 #include "ros/ros.h"
 #include <stdlib.h>
 #include <time.h>
+#include <sensor_msgs/Imu.h>
 #include <nav_msgs/Odometry.h>
 #include <math.h>
 
-#define WHEEL_RADIUS 0.024
-#define WHEELS_DISTANCE 0.25
 
 const std::string PREFIX_MSG = "Mirela";
 
@@ -22,6 +21,13 @@ struct Velocity {
 	double angular;
 };
 
+struct Imu {
+	double x;
+	double y;
+	double z;
+	double w;
+};
+
 struct Pose {
 	double x;
 	double y;
@@ -30,6 +36,7 @@ struct Pose {
 
 struct Velocity baseVelocity = {0,0};
 struct Pose basePose = {0,0,0};
+struct Imu imu = {0,0,0,0};
 
 float randomNumber(float Min, float Max)
 {
@@ -38,7 +45,14 @@ float randomNumber(float Min, float Max)
 void commandVelocityCallback(const geometry_msgs::Twist& msg)
 {
 	baseVelocity.linear = msg.linear.x;
-	baseVelocity.angular = msg.angular.z;	
+//	baseVelocity.angular = msg.angular.z;	
+}
+
+void commandImuCallback(const sensor_msgs::Imu& msg) {
+	imu.x = msg.orientation.x;
+	imu.y = msg.orientation.y;
+	imu.z = msg.orientation.z;
+	imu.w = msg.orientation.w;
 }
 
 int main(int argc, char **argv)
@@ -49,7 +63,8 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	ros::Publisher odometry_msg_pub = n.advertise<nav_msgs::Odometry>("Mirela/wheels_odom", 1);
 	ros::Subscriber cmd_vel_sub = n.subscribe("Mirela/cmd_vel", 1000, &commandVelocityCallback);
-	ros::Rate loop_rate(100);
+	ros::Subscriber cmd_imu_sub = n.subscribe("Mirela/imu", 1000, &commandImuCallback);
+	ros::Rate loop_rate(200);
 
 	odometry_msg.header.frame_id = "Mirela_odom";
 	odometry_msg.child_frame_id = "Mirela_base";
@@ -65,9 +80,11 @@ int main(int argc, char **argv)
 
 
 	double time_last = 0;	
-
+	double last_theta = 0;
+	double theta;
 	while (ros::ok()) {
-		
+		theta = atan2(2*(imu.w * imu.z + imu.x*imu.y), (1-2*(imu.y*imu.y+imu.z*imu.z)));	
+	
 		/*** Calculo da posicao e da orientacao
 			basePose.x = basePose.x + time_elapsed * WHEEL_RADIUS * baseVelocity.linear * cos(basePose.theta);
 			basePose.y = basePose.y + time_elapsed * WHEEL_RADIUS * baseVelocity.linear * sin(basePose.theta);
@@ -76,38 +93,41 @@ int main(int argc, char **argv)
 		double time_now = ros::Time::now().toSec();
 		double time_elapsed = time_now - time_last;
 		time_last = time_now;
-		double error_linear = randomNumber(-0.001, 0.001);
-		double error_angular = randomNumber(-0.005, 0.005);	
+		double error_linear = randomNumber(-0.0005, 0.0005);
+		//double error_angular = randomNumber(-0.002, 0.002);	
 		double linear = 0;
-		double angular = 0;
+		//double angular = 0;
 		if(baseVelocity.linear != 0.0)
 			linear = baseVelocity.linear + error_linear;	
-		if(baseVelocity.angular != 0.0)
-			angular = baseVelocity.angular + error_angular;
+	//	if(baseVelocity.angular != 0.0)
+	//		angular = baseVelocity.angular + error_angular;
 		
-		basePose.x = basePose.x + time_elapsed * linear * cos(basePose.theta);
-		basePose.y = basePose.y + time_elapsed * linear * sin(basePose.theta);
-		basePose.theta = basePose.theta + time_elapsed * angular;
+		basePose.x = basePose.x + time_elapsed * linear * cos(theta);
+		basePose.y = basePose.y + time_elapsed * linear * sin(theta);
+	//	basePose.theta = basePose.theta + time_elapsed * angular;
+		
 
 		odometry_msg.twist.twist.linear.x = linear;
-		odometry_msg.twist.twist.angular.z = angular;
+		odometry_msg.twist.twist.angular.z = 0;
 		odometry_msg.twist.twist.linear.y = 0; //baseVelocity.linear;
-		odometry_msg.twist.twist.linear.z = 0; randomNumber(-0.005, 0.005);
+		odometry_msg.twist.twist.linear.z = baseVelocity.angular; //randomNumber(-0.005, 0.005);
 
 
 		odometry_msg.pose.pose.position.x = basePose.x;
 		odometry_msg.pose.pose.position.y = basePose.y;
-		odometry_msg.pose.pose.position.z = 0.0;//pose.theta *180/3.1415;
+		odometry_msg.pose.pose.position.z = 0.0;//0.0;//pose.theta *180/3.1415;
 		odometry_msg.pose.pose.orientation.x = 0.0;//goal_vel.angular[2];
 		odometry_msg.pose.pose.orientation.y = 0.0;//dxl_wb.itemRead(DXL_ID, "Present_Position");
-		odometry_msg.pose.pose.orientation.z = sin(basePose.theta/2.0);
-		odometry_msg.pose.pose.orientation.w = cos(basePose.theta/2.0);
+		odometry_msg.pose.pose.orientation.z = imu.z;//sin(basePose.theta/2.0);
+		odometry_msg.pose.pose.orientation.w = imu.w;//cos(basePose.theta/2.0);
 		//odometry_msg.pose.covariance[0]  = 0.2; ///< x
 		//odometry_msg.pose.covariance[7]  = 0.2; ///< y
 		//odometry_msg.pose.covariance[35] = 0.4; ///< yaw
 		odometry_msg.header.stamp = ros::Time::now();
 
 		odometry_msg_pub.publish(odometry_msg);
+
+		last_theta = theta;
 
 		ros::spinOnce();
 		loop_rate.sleep();
